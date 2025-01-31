0e7c12f87a4531b87b7a5bc32d76df8ba2cc267a
seowas89 <seowaseem89@gmail.com>
2025-01-31 21:21:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import re
+import random
 
 # Function to simplify text
 def simplify_text(text):
     # Replace complex words with simpler ones
     word_replacements = {
-        "utilize": "use",
-        "demonstrate": "show",
-        "comprehend": "understand",
-        "facilitate": "help",
-        "terminate": "end"
+        "utilize": ["use", "make use of", "take advantage of"],
+        "demonstrate": ["show", "prove", "illustrate"],
+        "comprehend": ["understand", "get", "grasp"],
+        "facilitate": ["help", "assist", "make easier"],
+        "terminate": ["end", "stop", "finish"]
     }
     
-    # Replace complex words
-    for word, replacement in word_replacements.items():
-        text = re.sub(rf"\b{word}\b", replacement, text, flags=re.IGNORECASE)
+    # Replace complex words with randomized options
+    for word, replacements in word_replacements.items():
+        text = re.sub(rf"\b{word}\b", random.choice(replacements), text, flags=re.IGNORECASE)
     
     # Break down long sentences
     sentences = re.split(r'(?<=[.!?])\s+', text)
@@ -29,13 +30,18 @@ def simplify_text(text):
     # Join sentences back together
     simplified_text = " ".join(simplified_sentences)
     
-    # Add conversational tone
-    simplified_text = simplified_text.replace("It is", "It's").replace("You are", "You're")
+    # Add conversational tone and variability
+    simplified_text = simplified_text.replace("It is", random.choice(["It's", "This is", "That is"]))
+    simplified_text = simplified_text.replace("You are", random.choice(["You're", "You are", "You happen to be"]))
+    
+    # Add filler words and contractions for natural flow
+    simplified_text = simplified_text.replace("very", random.choice(["super", "really", "pretty"]))
+    simplified_text = simplified_text.replace("important", random.choice(["key", "crucial", "vital", "a big deal"]))
     
     return simplified_text
 
 # Streamlit App
-st.title("Text Simplifier")
+st.title("Human-Like Text Simplifier")
 
 # Input text area
 input_text = st.text_area("Enter your text here:", height=150)
